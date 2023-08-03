0c5b60cacbacaeb9a0d1404ca8e95b117af707ef
christoboucher <christo@jennyinternet.co.za>
2023-08-03 13:25:22 +0000

    add install ssh into Dockerfile

--- docker/build_and_push.s
+++ docker/build_and_push.s
@@ -3,4 +3,4 @@
 #need to be logged into docker to push to fermionms
 
 docker build -t christoboucher/db-tools -f docker/Dockerfile .
-# docker push christoboucher/db-tools
+docker push christoboucher/db-tools
